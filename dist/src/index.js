297d3d8b46968c8e979f9e3843cbb41e3b3c49b0
uioz <hzxbpld@163.com>
2019-02-13 13:44:32 +0800

    设置TypeScript - target:esnext 来使用Node10的新特性

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,19 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __asyncValues = (this && this.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./utils/index");
 /**
@@ -43,25 +28,12 @@ exports.WriteOptions = {
  * 工具函数将可以读流过程抽象为一个Promise
  * @param stream 可读流
  */
-function StreamReadAsync(stream) {
-    return __awaiter(this, void 0, void 0, function* () {
-        var e_1, _a;
-        const buffers = [];
-        try {
-            for (var _b = __asyncValues(stream), _c; _c = yield _b.next(), !_c.done;) {
-                const chunk = _c.value;
-                buffers.push(chunk);
-            }
-        }
-        catch (e_1_1) { e_1 = { error: e_1_1 }; }
-        finally {
-            try {
-                if (_c && !_c.done && (_a = _b.return)) yield _a.call(_b);
-            }
-            finally { if (e_1) throw e_1.error; }
-        }
-        return Buffer.concat(buffers);
-    });
+async function StreamReadAsync(stream) {
+    const buffers = [];
+    for await (const chunk of stream) {
+        buffers.push(chunk);
+    }
+    return Buffer.concat(buffers);
 }
 exports.StreamReadAsync = StreamReadAsync;
 /**
